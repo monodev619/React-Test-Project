3cc5080b3b47a9b35cc4107a43984184e106e77c
xuebao555 <xuebao555@gmail.com>
2016-10-11 11:10:43 +0800

    webpack config files

---
+++ webpack.config.js
@@ -0,0 +1,67 @@
+var path = require('path');
+var webpack = require('webpack');
+
+module.exports = {
+    entry: [
+        'babel-polyfill',
+        './node_modules/bootstrap/dist/js/bootstrap.min.js',
+        './node_modules/bootstrap/dist/css/bootstrap.min.css',
+        './node_modules/font-awesome/css/font-awesome.min.css',
+        './app/index.js',
+    ],
+
+    output: {
+        path: __dirname + '/public/',
+        filename: 'bundle.js'
+    },
+
+    module: {
+        loaders: [
+            {
+                test: /\.js$/,
+                loaders: ['babel?' + JSON.stringify({
+                    cacheDirectory: true,
+                    presets: ['es2015', 'react', 'stage-0']
+                })],
+                exclude: /node_modules/,
+            },
+            {
+                test: /\.css$/,
+                loader: 'style!css-loader'
+            },
+            {
+                test: /\.(ttf|eot|svg|woff(2)?)(\?[a-z0-9=&.]+)?$/,
+                loader : 'file-loader'
+            },
+            {
+                test: /bootstrap.+\.(jsx|js)$/,
+                loader: 'imports?jQuery=jquery,$=jquery,this=>window'
+            },
+            {
+                test: /\.(jpe?g|png|gif|svg)$/i,
+                loaders: [
+                    'file?hash=sha512&digest=hex&name=[hash].[ext]',
+                    'image-webpack?bypassOnDebug&optimizationLevel=7&interlaced=false'
+                ]
+            }
+        ]
+    },
+
+    resolve: {
+        root: path.resolve('./app')
+    },
+
+    plugins:[
+        new webpack.DefinePlugin({
+          'process.env':{
+            'NODE_ENV': JSON.stringify('production')
+          }
+        }),
+        new webpack.optimize.UglifyJsPlugin({
+          compress:{
+            warnings: true
+          }
+        })
+    ]
+
+};
---
+++ webpack.dev.config.js
@@ -0,0 +1,84 @@
+
+var webpack = require('webpack');
+var path = require('path');
+
+
+module.exports = {
+
+    entry: [
+        'babel-polyfill',
+        './node_modules/bootstrap/dist/js/bootstrap.min.js',
+        './node_modules/bootstrap/dist/css/bootstrap.min.css',
+        './node_modules/font-awesome/css/font-awesome.min.css',
+        './app/index.js',
+        'webpack-dev-server/client?http://0.0.0.0:3001',
+        'webpack/hot/only-dev-server'
+    ],
+
+    output: {
+        path: '/',
+        filename: 'bundle.js'
+    },
+    devtool: 'source-map',
+    devServer: {
+        hot: true,
+        filename: 'bundle.js',
+        publicPath: '/',
+        historyApiFallback: true,
+        contentBase: './public',
+        proxy: {
+            "*": "http://localhost:3000"
+        },
+        stats: {
+          // Config for minimal console.log mess.
+          assets: false,
+          colors: true,
+          version: false,
+          hash: false,
+          timings: false,
+          chunks: false,
+          chunkModules: false
+        }
+    },
+
+    plugins: [
+        new webpack.HotModuleReplacementPlugin()
+    ],
+
+    module: {
+        loaders: [
+            {
+                test: /\.js$/,
+                loaders: ['react-hot', 'babel?' + JSON.stringify({
+                    cacheDirectory: true,
+                    presets: ['es2015', 'react', 'stage-0']
+                })],
+                exclude: /node_modules/,
+            },
+            {
+                test: /\.css$/,
+                loader: 'style!css-loader'
+            },
+            {
+                test: /\.(ttf|eot|svg|woff(2)?)(\?[a-z0-9=&.]+)?$/,
+                loader : 'file-loader'
+            },
+            {
+                test: /bootstrap.+\.(jsx|js)$/,
+                loader: 'imports?jQuery=jquery,$=jquery,this=>window'
+            },
+            {
+                test: /\.(jpe?g|png|gif|svg)$/i,
+                loaders: [
+                    'file?hash=sha512&digest=hex&name=[hash].[ext]',
+                    'image-webpack?bypassOnDebug&optimizationLevel=7&interlaced=false'
+                ]
+            }
+        ]
+    },
+
+    resolve: {
+        root: path.resolve('./app')
+    }
+
+};
